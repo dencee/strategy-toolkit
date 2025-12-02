7af6c2b8d268057884173433920b573a799f39bd
Daniel Commins <dencee@gmail.com>
2025-12-01 20:34:19 -0800

    test deploy 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,           // default, OK
   nitro: {
-    preset: 'github-pages'
+    preset: 'github-pages',
   },
   app: {
     baseURL: process.env.NODE_ENV === 'production' ? '/strategy-toolkit/' : '/',
-  }
+  },
 })
