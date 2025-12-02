b7945ed6594545adbd27b18d49225a9b32a8df5d
Daniel Commins <dencee@gmail.com>
2025-12-01 16:02:17 -0800

    github workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  ssr: true,           // default, OK
+  nitro: {
+    preset: 'github-pages'
+  },
+  app: {
+    baseURL: '/strategy-toolkit/',     // important for GitHub Pages
+    buildAssetsDir: 'assets'      // optional but common
+  }
 })
