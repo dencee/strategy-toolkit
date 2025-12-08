afe0a8a49e71e161ab99daa384587c1edd220f23
Daniel Commins <dencee@gmail.com>
2025-12-08 01:37:23 -0800

    header fix

--- app/view/Footer.vue
+++ app/view/Footer.vue
@@ -4,13 +4,21 @@
 
 <template>
   <footer class="py-4 bg-white dark:bg-dark-900">
-    <div class="container-max section-padding">
+    <div class="container-max p-8">
       <div class="grid grid-cols-1 md:grid-cols-4 gap-8">
         <div>
-          <h3 class="text-lg font-semibold mb-4">External Links</h3> 
+          <h3 class="text-lg font-semibold mb-4">External Links</h3>
           <ul class="space-y-2">
-            <li><NuxtLink to="https://sites.google.com/view/daniels-math-corner" external target="_blank">Daniel's Math Corner</NuxtLink></li>
-            <li><NuxtLink to="https://github.com/dencee/strategy-toolkit" external target="_blank">Site Source Code</NuxtLink></li>
+            <li>
+              <NuxtLink to="https://sites.google.com/view/daniels-math-corner" external target="_blank"
+                class="text-gray-700 dark:text-slate-300 hover:text-blue-500 dark:hover:text-blue-300 transition-colors">
+                Daniel's Math Corner</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="https://github.com/dencee/strategy-toolkit" external target="_blank"
+                class="text-gray-700 dark:text-slate-300 hover:text-blue-500 dark:hover:text-blue-300 transition-colors">
+                Site Source Code</NuxtLink>
+            </li>
           </ul>
         </div>
       </div>
